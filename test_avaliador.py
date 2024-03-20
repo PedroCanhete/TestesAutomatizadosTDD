4ef74a46f32b4fcd9bc9e030eb1e1880eb47235b
Pedro Canhete <pedro.canhete98@gmail.com>
2024-03-20 16:51:16 -0300

    Criação Classes Testes Automatizados
    Criação e definição de classes para serem testadas e automatizadas. No exemplo utilizando um sisteminha de leilão de itens.

---
+++ test_avaliador.py
@@ -0,0 +1,74 @@
+from ClassesLeilao import Usuario, Lance, Leilao, Avaliador
+from unittest import TestCase
+
+
+class TestAvaliador(TestCase):
+    def setUp(self):
+        self.pedro = Usuario('Pedro')
+        self.lance_pedro = Lance(self.pedro, 100)
+        self.leilao = Leilao('Carro 0Km')
+
+    
+    
+    def teste_retorno_maior_menor_valor_lances_adicionados_ordem_crescente(self):
+        bibi = Usuario('Bibi')
+        lance_bibi = Lance(bibi, 150)
+
+        self.leilao.propoe(self.lance_pedro)
+        self.leilao.propoe(lance_bibi)
+
+        avaliador = Avaliador()
+        avaliador.avalia(self.leilao)
+
+        menor_valor_esperado = 100.00
+        maior_valor_esperado = 150.00
+
+        self.assertEqual(menor_valor_esperado, avaliador.menor_lance)
+        self.assertEqual(maior_valor_esperado, avaliador.maior_lance)
+        
+
+    def teste_retorno_maior_menor_valor_lances_adicionados_ordem_decrescente(self):
+        bibi = Usuario('Bibi')
+        lance_bibi = Lance(bibi, 150)
+        
+        self.leilao.propoe(lance_bibi)
+        self.leilao.propoe(self.lance_pedro)
+
+        avaliador = Avaliador()
+        avaliador.avalia(self.leilao)
+
+        menor_valor_esperado = 100.00
+        maior_valor_esperado = 150.00
+
+        self.assertEqual(menor_valor_esperado, avaliador.menor_lance)
+        self.assertEqual(maior_valor_esperado, avaliador.maior_lance)
+
+
+    def teste_retorno_maior_menor_valor_lance_unico(self):
+        self.leilao.propoe(self.lance_pedro)
+
+        avaliador = Avaliador()
+        avaliador.avalia(self.leilao)
+
+        self.assertEqual(100.0, avaliador.menor_lance)
+        self.assertEqual(100.0, avaliador.maior_lance)
+
+    def teste_retorno_maior_menor_valor_lances_multiplos(self):
+        bibi = Usuario('Bibi')
+        lance_bibi = Lance(bibi, 150)
+        juanito_jones = Usuario('juanito')
+        lance_juanito_jones = Lance(juanito_jones, 200.0)
+
+        self.leilao.propoe(lance_bibi)
+        self.leilao.propoe(self.lance_pedro)
+        self.leilao.propoe(lance_juanito_jones)
+
+        avaliador = Avaliador()
+        avaliador.avalia(self.leilao)
+
+        menor_valor_esperado = 100.0
+        maior_valor_esperado = 200.0
+
+        self.assertEqual(menor_valor_esperado, avaliador.menor_lance)
+        self.assertEqual(maior_valor_esperado, avaliador.maior_lance)
+
